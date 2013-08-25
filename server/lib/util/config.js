edca03e40df0ed1475eb0af565cb53e5385d0963
Reed Lauber <reedlauber@gmail.com>
2013-08-25 17:33:31 -0600

    Added agency-specific config.

--- server/lib/util/config.js
+++ server/lib/util/config.js
@@ -1,14 +1,20 @@
 var config = {},
 	fs = require('fs');
-	local = {};
+	local = {},
+	agency = {};
 
-if(fs.existsSync(__dirname + '/../../local.json')) {
-	local = require('../../local.json');	
+if(fs.existsSync(__dirname + '/../../../config/local.json')) {
+	local = require('../../../config/local.json');
+}
+
+if(fs.existsSync(__dirname + '/../../../config/agency.json')) {
+	local = require('../../../config/agency.json');
 }
 
 config.debug_assets = process.env.DEBUG_ASSETS || local.debug_assets;
-config.database_url = process.env.DATABASE_URL || local.database_url;
 config.mail_username = process.env.MAIL_USERNAME || local.mail_username;
 config.mail_password = process.env.MAIL_PASSWORD || local.mail_password;
+
+config.database_url = process.env.DATABASE_URL || local.database_url || agency.database_url;
 
 module.exports = config;

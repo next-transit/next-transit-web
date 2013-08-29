c9765685b7e899d0e19f0eb32ef66deb8abb4b79
Reed Lauber <reedlauber@gmail.com>
2013-08-29 14:54:33 -0600

    Updated prod database url.

--- server/lib/util/config.js
+++ server/lib/util/config.js
@@ -10,7 +10,7 @@ config.debug_assets = process.env.DEBUG_ASSETS || local.debug_assets;
 config.mail_username = process.env.MAIL_USERNAME || local.mail_username;
 config.mail_password = process.env.MAIL_PASSWORD || local.mail_password;
 
-config.database_url = process.env.DATABASE_URL || local.database_url[local.agency];
+config.database_url = process.env.HEROKU_POSTGRESQL_YELLOW_URL || local.database_url[local.agency];
 
 config.agency = process.env.AGENCY || local.agency;
 

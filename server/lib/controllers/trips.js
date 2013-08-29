e8bb2075902877490bd6d850e0abedd1b2cecc45
Reed Lauber <reedlauber@gmail.com>
2013-08-29 11:29:48 -0600

    Updated realtime markers to only display for non-subway.

--- server/lib/controllers/trips.js
+++ server/lib/controllers/trips.js
@@ -16,14 +16,14 @@ ctrl.action('index', function(req, res, callback) {
 
 	var offset_prev = offset - 5,
 		offset_next = offset + 5;
-		
+
 	display_trips.get_by_time(req.agency.id, req.route.is_rail, req.route.route_id, req.direction_id, req.from_stop.stop_id, offset, req.to_stop, function(trips) {
 		callback(view, {
-			title:route.route_short_name + ' - ' + direction.direction_name, 
-			trips:trips, 
+			title: route.route_short_name + ' - ' + direction.direction_name, 
+			trips: trips, 
 			direction_id_rev: req.direction_id === '0' ? '1' : '0',
-			offset_prev:offset_prev, 
-			offset_next:offset_next
+			offset_prev: offset_prev, 
+			offset_next: offset_next
 		});
 	});
 });

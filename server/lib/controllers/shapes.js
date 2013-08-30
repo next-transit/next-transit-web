ba6561ffa5997acc170f4c97b8c6825c3ecbac1e
Reed Lauber <reedlauber@gmail.com>
2013-08-30 10:35:20 -0600

    Fixed route shapes missing agency id.

--- server/lib/controllers/shapes.js
+++ server/lib/controllers/shapes.js
@@ -3,9 +3,9 @@ var promise = require('promise'),
 	routes = require('../models/routes'),
 	shapes = require('../models/shapes');
 
-function get_simplified_shape_by_route_id(route_id) {
+function get_simplified_shape_by_route_id(agency_id, route_id) {
 	return new promise(function(resolve, reject) {
-		shapes.where('agency_id = ? AND lower(route_id) = ?', [req.agency.id, route_id.toLowerCase()])
+		shapes.where('agency_id = ? AND lower(route_id) = ?', [agency_id, route_id.toLowerCase()])
 			.orders('shape_pt_sequence')
 			.done(function(points) {
 				var simplified_shape = [];
@@ -13,7 +13,7 @@ function get_simplified_shape_by_route_id(route_id) {
 					simplified_shape.push([parseFloat(point.shape_pt_lat), parseFloat(point.shape_pt_lon)]);
 				});
 				
-				routes.where('agency_id = ? AND (lower(route_id) = ? OR lower(route_short_name) = ?)', [req.agency.id, route_id.toLowerCase(), route_id.toLowerCase()]).first(function(route) {
+				routes.where('agency_id = ? AND (lower(route_id) = ? OR lower(route_short_name) = ?)', [agency_id, route_id.toLowerCase(), route_id.toLowerCase()]).first(function(route) {
 					routes.process(route, function(processed) {
 						processed.points = simplified_shape;
 						resolve(processed);
@@ -23,7 +23,7 @@ function get_simplified_shape_by_route_id(route_id) {
 	});
 }
 
-function get_shapes_for_routes(route_results, callback) {
+function get_shapes_for_routes(agency_id, route_results, callback) {
 	if(!route_results || !route_results.length) {
 		callback([]);
 		return;
@@ -34,7 +34,7 @@ function get_shapes_for_routes(route_results, callback) {
 
 	route_results.forEach(function(result) {
 		if(result.route_id) {
-			promises.push(get_simplified_shape_by_route_id(result.route_id));
+			promises.push(get_simplified_shape_by_route_id(agency_id, result.route_id));
 		}
 	});
 
@@ -47,7 +47,7 @@ ctrl.action('index', { json:true }, function(req, res, callback) {
 	if(req.route_id) {
 		var route_id = req.route_id.toLowerCase(), route = req.route;
 
-		get_simplified_shape_by_route_id(route_id).done(function(simplified_shape) {
+		get_simplified_shape_by_route_id(req.agency.id, route_id).done(function(simplified_shape) {
 			callback(simplified_shape);
 		});
 	} else {
@@ -67,7 +67,7 @@ ctrl.action('bbox', { json:true }, function(req, res, callback) {
 			.select('distinct route_id')
 			.where('agency_id = ? AND shape_pt_lon > ? AND shape_pt_lon < ? AND shape_pt_lat > ? AND shape_pt_lat < ?', [req.agency.id, left, right, bottom, top])
 			.done(function(results) {
-				get_shapes_for_routes(results, function(routes) {
+				get_shapes_for_routes(req.agency.id, results, function(routes) {
 					callback({ routes:routes });
 				});
 			});

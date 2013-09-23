bb6fcb2b4ce76dc06f40130599e01817125bdc28
Reed Lauber <reedlauber@gmail.com>
2013-09-23 13:49:49 -0600

    Build 2.2.0

--- app/build/app.js
+++ app/build/app.js
@@ -596,9 +596,9 @@ nextsepta.module('nextsepta').service('map_locate', ['module', 'data', 'history'
 		map_ctrl.clear_vectors();
 
 		results.forEach(function(result) {
-			if(result.points.length) {
+			if(result.shapes.length) {
 
-				var route_layer = map_ctrl.add_vector(result.points, result.color, 0.45 );
+				var route_layer = map_ctrl.add_vector(result.shapes, result.color, 0.45);
 
 				route_layer.on('click', function(evt) {
 					history.push('/' + result.route_type_slug + '/' + result.slug);
@@ -797,16 +797,18 @@ nextsepta.module('nextsepta').service('map_vectors', ['data', function(data) {
 		}
 	}
 
-	function add_shape(points, color, opacity) {
-		return L.polyline(points, {
-			color: color || '#a33', 
-			opacity: opacity || 0.65
-		}).addTo(layer_group);
+	function add_shapes(shapes, color, opacity) {
+		shapes.forEach(function(shape) {
+			L.polyline(shape, {
+				color: color || '#a33', 
+				opacity: opacity || 0.65
+			}).addTo(layer_group);
+		});
 	}
 
 	function add_route(route_type, route_id, fit_to_route) {
 		data.get(['', route_type, route_id, 'shape'].join('/'), function(resp) {
-			add_shape(resp.points, resp.color);
+			add_shapes(resp.shapes, resp.color);
 			if(fit_to_route) {
 				fit_to_last();
 			}
@@ -826,7 +828,7 @@ nextsepta.module('nextsepta').service('map_vectors', ['data', function(data) {
 	}
 
 	return {
-		add: add_shape,
+		add: add_shapes,
 		add_route: add_route,
 		clear: clear_layers,
 		set_map_ctrl: function(ctrl, $map_elem) {

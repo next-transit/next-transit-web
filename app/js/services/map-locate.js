49b9cb187ec31465345a405414776fa508d9ffea
Reed Lauber <reedlauber@gmail.com>
2014-03-11 19:03:38 -0700

    Reactivated routes by location.

--- app/js/services/map-locate.js
+++ app/js/services/map-locate.js
@@ -106,7 +106,7 @@ nextsepta.module('nextsepta').service('map_locate', ['module', 'data', 'history'
 	}
 
 	function locate() {
-		// active = true;
+		active = true;
 		// render_user_location(39.926312796934674, -75.16697645187378);
 		// return;
 
